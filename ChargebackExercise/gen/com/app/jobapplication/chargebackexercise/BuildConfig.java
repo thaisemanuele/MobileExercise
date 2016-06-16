4eb205653c71fd1ba079298c854ddab2759ca606
thatitis <thatitis@gmail.com>
2016-06-15 21:41:37 -0300

    Connector Class

---
+++ ChargebackExercise/gen/com/app/jobapplication/chargebackexercise/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.app.jobapplication.chargebackexercise;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
